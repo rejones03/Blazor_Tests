e042f4198c1c5485a27c3894bfb9faed0fc0f972
Ronald Jones <ronald.jones@mshp.dps.mo.gov>
2019-12-24 14:47:07 -0600

    initial checkin

---
+++ BethanysPieShoppeHRM.Server/BethanysPieShopHRM.Server/Services/EmployeeDataService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using BethanysPieShopHRM.Shared;
+
+namespace BethanysPieShopHRM.Server.Services
+{
+    public class EmployeeDataService : IEmployeeDataService
+    {
+        private readonly HttpClient _httpClient;
+
+        public EmployeeDataService(HttpClient httpclient)
+        {
+            _httpClient = httpclient;
+        }
+
+        public Task<Employee> AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteEmployee(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllEmployees()
+        {
+            return await JsonSerializer.DeserializeAsync<IEnumerable<Employee>>
+                (await _httpClient.GetStreamAsync($"api/employee"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+
+        public async Task<Employee> GetEmployeeDetails(int employeeId)
+        {
+            return await JsonSerializer.DeserializeAsync<Employee>
+                (await _httpClient.GetStreamAsync($"api/employee/{employeeId}"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+
+        public Task UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BethanysPieShoppeHRM.Server/getting-started-blazor/03/demos/Demos/DataBindingSample/DataBindingSample/Pages/IndexBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace DataBindingSample.Pages
+{
+    public class IndexBase: ComponentBase
+    {
+        public Employee Employee { get; set; }
+        protected override Task OnInitializedAsync()
+        {
+            Employee = new Employee
+            {
+                FirstName = "Bethany",
+                LastName = "Smith",
+            };
+
+            return base.OnInitializedAsync();
+        }
+
+        public void Button_Click()
+        {
+            Employee.FirstName = "Gill";
+
+        }
+    }
+}
